692756bba008fed09f0b70f3db54506169c77e55
Jeff Lee <jeffomatic@gmail.com>
2015-05-11 14:53:58 -0700

    Fix HttpUtility error handling
    - Call callback as soon as errors are detected
    - Use more verbose error messages
    - Handle JSON decoding correctly
    
    Reviewed by @ryandotsmith
    Closes #20, #21

--- http-utility.js
+++ http-utility.js
@@ -9,15 +9,7 @@ var PEM = fs.readFileSync(path.join(__dirname, './chain.pem'));
 module.exports = HttpUtility;
 
 function HttpUtility(c) {
-  if(c.auth == null) {
-    c.auth = "GUEST-TOKEN";
-  };
-  this.auth = c.auth;
-
-  if (c.url == null) {
-    c.url = URL;
-  };
-  this.url = c.url;
+  this.url = c.url || URL;
 }
 
 HttpUtility.prototype.makeRequest = function(method, path, body, options, cb) {
@@ -37,14 +29,31 @@ HttpUtility.prototype.makeRequest = function(method, path, body, options, cb) {
     r['qs'] = options;
   }
   request(r, function(err, resp, body) {
+    if (err) {
+      return cb(err);
+    }
+
     if (Math.floor(resp.statusCode / 100) != 2) {
-      err = body;
+      err = new Error("Chain SDK error: bad status code " + resp.statusCode.toString() + ". See 'resp' property for more detail.");
+      err.resp = resp;
+      return cb(err);
     }
+
     if(usingJson) {
-      cb(err, body);
-    } else {
-      cb(err, JSON.parse(body));
+      return cb(err, body);
     }
+
+    var parsed;
+    try {
+      parsed = JSON.parse(body);
+    } catch (e) {
+      err = new Error("Chain SDK error: could not decode JSON response. See 'error' and 'resp' properties for more detail.");
+      err.error = e;
+      err.resp = resp;
+      return cb(err);
+    }
+
+    cb(null, parsed);
   });
 };
 
---
+++ test/http-utility.test.js
@@ -0,0 +1,45 @@
+var assert = require('assert'),
+  HttpUtility = require('../http-utility.js'),
+  nock = require('nock');
+
+describe('HttpUtility', function () {
+
+  beforeEach(function () {
+    this.client = new HttpUtility({});
+  });
+
+  afterEach(function () {
+    nock.cleanAll();
+  });
+
+  describe('#makeRequest()', function () {
+
+    it('should not error on a valid JSON response', function (done) {
+      nock('https://api.chain.com').get('/').reply(200, '{}');
+      this.client.makeRequest('GET', '/', null, null, function (err) {
+        assert(!err);
+        done();
+      });
+    });
+
+    it('should return an error on non-200 response', function (done) {
+      nock('https://api.chain.com').get('/').reply(400, '');
+      this.client.makeRequest('GET', '/', null, null, function (err) {
+        assert(err);
+        assert(err.message.match(/bad status code/));
+        done();
+      });
+    });
+
+    it('should return an error on an unparseable JSON response', function (done) {
+      nock('https://api.chain.com').get('/').reply(200, 'NOT JSON');
+      this.client.makeRequest('GET', '/', null, null, function (err) {
+        assert(err);
+        assert(err.message.match(/could not decode JSON/));
+        done();
+      });
+    });
+
+  });
+
+});
